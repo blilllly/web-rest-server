a4ddbe744cf541f0df302cb1fbe8c2a4a14482eb
Billy Alvear <alvearbilly@gmail.com>
2026-04-28 23:47:09 -0500

    createServer http

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import http from 'http';
+import fs from 'fs';
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
-  res.write('Hola Mundo');
-  res.end();
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write(`<h1>URL ${req.url}</h1>`);
+  // res.end();
+
+  // const data = { name: 'Billy Alvear', age: 27, city: 'New York' };
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.end(JSON.stringify(data));
+
+  if (req.url === '/') {
+    const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlFile);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end();
+  }
 });
 
 server.listen(8080, () => {
