3f7a4f6a91c1ea8185b82edcddb6ac8383da9bf0
Billy Alvear <alvearbilly@gmail.com>
2026-05-02 16:45:02 -0500

    CRUD - Read

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
+import { TodoRoutes } from './todos/routes';
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get('/api/todos', (req, res) => {
-      res.json([
-        { id: 1, text: 'Buy Milk', createdAt: new Date() },
-        { id: 2, text: 'Buy bread', createdAt: null },
-        { id: 3, text: 'Buy butter', createdAt: new Date() },
-      ]);
-    });
+    router.use('/api/todos', TodoRoutes.routes);
 
     return router;
   }
